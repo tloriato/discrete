908472750f10fd1762985bf8723e9bc0930bc4f9
Tiago Loriato <tloriatosimoes@gmail.com>
2018-10-05 15:19:37 -0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -29,23 +29,36 @@ def populateFromFile(file, nodes):
   for i in range(nodes):
     matrix[i] = createMask(graph[i])
   return matrix
-  
+
+# Receives an empty matrix and fills it 
+# with the combinations of count of the list element
+# i.e: list = [1,2,3] and count = 2 ->
+#      matrix = [[1,2], [1,3], [2,3]]
+def fillMatrixOfSubsets(matrix, list, count, prefix=[]):
+  if count == 0:
+    matrix.append(prefix)
+    return
+  else:
+    for x in range(len(list)):
+      fillMatrixOfSubsets(matrix, list[x+1:], count - 1, prefix + [list[x]])
+
+def friendlyGroup(array):
+  return False
+
 # Finds the biggest group of friends
 # given an Adjacency Matrix implemented in
 # an array with bitmap
-def findFriends(matrix):
-  size = len(matrix)
-  friends = []
-  for i in range(size, 0, -1):
-    temp = findFriendsOfN(matrix, i)
-    if len(temp) > len(friends):
-      friends = temp.copy()
-  return friends
-
-def findFriendsOfN(matrix, i):
-  friends = []
-  # do something #
-  return friends
+def findMostFriendlyGroup(matrix):
+  largestGroup = []
+  for x in range(len(matrix), 0, -1):
+    subset = []
+    fillMatrixOfSubsets(subset, matrix, x)
+    for sset in subset:
+      friends = friendlyGroup(sset)
+      if friends == True:
+        largestGroup = sset.copy()
+        break;
+  return 0
 
 
 community = open("comunidade.txt", 'r')
@@ -53,4 +66,5 @@ community = open("comunidade.txt", 'r')
 nodes = int(community.readline())
 matrix = populateFromFile(community, nodes)
 
-largestSubGroup = findFriends(matrix)
+largestSubGroup = findMostFriendlyGroup(matrix)
+
