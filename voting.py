5835e9313eb9333efed6fa8864d7c85098b8a31d
Tiago Loriato <tloriatosimoes@gmail.com>
2018-10-10 16:02:41 -0300

    1.0

--- voting.py
+++ voting.py
@@ -1,25 +1,45 @@
-def iVoting(win, loo, maxd, sc=0):
+def calculateVotes(win, loo, maxd, sc=0):
   if (win, loo, maxd, sc) in hashTable:
     return hashTable[win, loo, maxd, sc]
   if win == 0 and loo == 0:
     return 1
   else:
     if loo == 0:
-      hashTable[win, loo, maxd, sc] = 0 + iVoting(win - 1, loo, maxd, sc + 1)
+      hashTable[win, loo, maxd, sc] = 0 + calculateVotes(win - 1, loo, maxd, sc + 1)
     else:
       if sc + 1 < maxd and win > 0:
-        hashTable[win, loo, maxd, sc] = iVoting(win - 1, loo, maxd, sc + 1) + iVoting(win, loo - 1, maxd, sc - 1)
+        hashTable[win, loo, maxd, sc] = calculateVotes(win - 1, loo, maxd, sc + 1) + calculateVotes(win, loo - 1, maxd, sc - 1)
       else:
-        hashTable[win, loo, maxd, sc] = 0 + iVoting(win, loo - 1, maxd, sc - 1)
+        hashTable[win, loo, maxd, sc] = 0 + calculateVotes(win, loo - 1, maxd, sc - 1)
     return hashTable[win, loo, maxd, sc]
 
+def generateVotes(win, loo, maxd, arr=[], sc=0):
+  global outFile
+  if win == 0 and loo == 0:
+    outFile.write(''.join(arr) + '\n')
+    return 1
+  else:
+    if loo == 0:
+      return generateVotes(win - 1, loo, maxd, arr + ['W'], sc + 1)
+    else:
+      if sc + 1 < maxd and win > 0:
+        return generateVotes(win - 1, loo, maxd, arr + ['W'], sc + 1) + generateVotes(win, loo - 1, maxd, arr + ['L'], sc - 1)
+      else:
+        return generateVotes(win, loo - 1, maxd, arr + ['L'], sc - 1)
+
+
+## Interface Function ##
 def voting (a, b, c):
+  global outFile
   winnerVotes = b + ((a - b)/2)
   looserVotes = a - winnerVotes
-  maxDifference = c
-  if not winnerVotes.is_integer() or not looserVotes.is_integer() or c <= b:
-      raise ValueError('Wrong parameters!')
-  return iVoting(winnerVotes, looserVotes, maxDifference)
+  results = calculateVotes(winnerVotes, looserVotes, c)
+  if results > 1000:
+    outFile.write(str(results))
+  else:
+    generateVotes(winnerVotes, looserVotes, c)
+
+
 
 ## Main ##
 hashTable = {} 
@@ -31,14 +51,12 @@ c = int(votes.readline())
 
 votes.close()
 
-out = open("voting-saida.txt", 'w')
+outFile = open("voting-saida.txt", 'w')
 
 x = voting(a, b, c)
 
-print(a, b, c)
-print(x)
+outFile.close()
 
-out.close()
+## End Main ##
 
-#print("Testando voting(70, 20, 30) :D")
-#print(voting(70,20,30))
+## 70 20 30
